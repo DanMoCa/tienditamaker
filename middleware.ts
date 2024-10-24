fd64d42cd3f69ba2313418428df3ee04917f08b3
Saoko1x <jemg2510@gmail.com>
2024-10-23 23:25:13 -0600

    Refactor middleware to include subdomain in request headers

--- middleware.ts
+++ middleware.ts
@@ -3,12 +3,15 @@ import { getToken } from "next-auth/jwt";
 
 export async function middleware(req: NextRequest) {
   const token = await getToken({ req });
+  const { hostname } = req.nextUrl;
+  const subdomain = hostname.split(".")[0];
 
   // Verifica si el token existe
   if (!token && req.nextUrl.pathname === "/dashboard") {
     // Redirige a la página de inicio si no hay sesión
     return NextResponse.redirect(new URL("/", req.url));
   }
+  req.headers.set("x-subdomain", subdomain);
 
   return NextResponse.next(); // Permite continuar con la solicitud
 }
